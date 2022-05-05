e1ed3a7aa1326b846d10ea272e0da8dd63611205
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-04 19:57:23 -0500

    Implemented quicksort.

--- src/main/kotlin/sortingAlgorithms/QuickSort.kt
+++ src/main/kotlin/sortingAlgorithms/QuickSort.kt
@@ -1,5 +1,30 @@
 package sortingAlgorithms
 
 fun quickSort( unsortedArray: Array< Int >, startIndex: Int, endIndex: Int ) {
+    if( startIndex >= endIndex )
+        return
 
+    val pivotIndex = Partition( unsortedArray, startIndex, endIndex )
+    quickSort( unsortedArray, startIndex, pivotIndex - 1 )
+    quickSort( unsortedArray, pivotIndex + 1, endIndex )
+}
+
+fun Partition( unsortedArray: Array< Int >, startIndex: Int, endIndex: Int ) : Int {
+    val pivotValue = unsortedArray[ endIndex ]
+    var currentPivotIndex = startIndex - 1
+
+    for( currentIndex in unsortedArray.indices ) {
+        if( unsortedArray[ currentIndex ] < pivotValue ) {
+            currentPivotIndex++
+            unsortedArray[ currentIndex ] = unsortedArray[ currentPivotIndex ].also {
+                unsortedArray[ currentPivotIndex ] = unsortedArray[ currentIndex ]
+            }
+        }
+    }
+
+    currentPivotIndex++
+    unsortedArray[ currentPivotIndex ] = unsortedArray[ endIndex ].also {
+        unsortedArray[ endIndex ] = unsortedArray[ currentPivotIndex ]
+    }
+    return currentPivotIndex
 }
