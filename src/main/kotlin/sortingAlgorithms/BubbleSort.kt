22c9872d7dbe0bbb1f15c47a9314d43924e2ae2c
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-11 15:57:56 -0500

    Implemented bubble sort.

--- src/main/kotlin/sortingAlgorithms/BubbleSort.kt
+++ src/main/kotlin/sortingAlgorithms/BubbleSort.kt
@@ -1,5 +1,3 @@
-package sortingAlgorithms
-
 /*
 * Bubble Sort:
 *
@@ -8,4 +6,16 @@ package sortingAlgorithms
 * until the list is sorted. Bubble sort is not a practical
 * sorting algorithm.
 * */
+package sortingAlgorithms
 
+fun bubbleSort( unsortedArray: Array< Int > ) {
+    for( count in unsortedArray.indices ) {
+        for( swapIndex in 0 until unsortedArray.size - count - 1 ) {
+            if( unsortedArray[ swapIndex ] > unsortedArray[ swapIndex + 1 ] ) {
+                unsortedArray[ swapIndex ] = unsortedArray[ swapIndex + 1 ].also {
+                    unsortedArray[ swapIndex + 1 ] = unsortedArray[ swapIndex ]
+                }
+            }
+        }
+    }
+}
