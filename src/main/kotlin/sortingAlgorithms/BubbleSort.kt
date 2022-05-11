59c47175c4789a4b619a14d0fb097458dd015d78
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-11 15:41:37 -0500

    Created file BubbleSort.kt

---
+++ src/main/kotlin/sortingAlgorithms/BubbleSort.kt
@@ -0,0 +1,11 @@
+package sortingAlgorithms
+
+/*
+* Bubble Sort:
+*
+*A sorting algorithm that compares two adjacent elements and
+* swaps them if they are in the wrong order. This is repeated
+* until the list is sorted. Bubble sort is not a practical
+* sorting algorithm.
+* */
+
